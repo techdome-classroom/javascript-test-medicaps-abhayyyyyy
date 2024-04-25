d6f8c324ca78083d3743a1bd03639561b1388820
abhay <142.abhay@gmail.com>
2024-04-25 13:50:34 +0530

    Doneeeee

--- program2.js
+++ program2.js
@@ -1,7 +1,17 @@
 function smallestMissingPositiveInteger(nums) {
-
+  // console.log(typeof(nums))
   // Implement the function smallest_missing_positive_integer
+  const numSet = new Set(nums.filter(num => num > 0));
+
+    let smallestMissing = 1;
+    while (numSet.has(smallestMissing)) {
+        smallestMissing++;
+    }
+
+    return smallestMissing;
+    // return 0;
 
 }
+
 
 module.exports = smallestMissingPositiveInteger;
